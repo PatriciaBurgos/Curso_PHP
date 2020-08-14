805dc17b1cdf1347033202e6d3dafd136b1d08da
PatriciaBurgos <patriciaburgos.1999@gmail.com>
2020-08-14 20:58:06 +0200

    Variables

---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+
+	$cadena = "Soy una cadena... Hola!";
+	$numero = 2;
+	$decimal = 4.5;
+	$caracter = 'L';
+
+	echo "Cadena: " . $cadena . "<br />";
+	echo "Numero: " . $numero . "<br />";
+	echo "Decimal: " . $decimal . "<br />";
+	echo "Caracter: " . $caracter . "<br />";
+
+
+?>
