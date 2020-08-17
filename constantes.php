504acf27cf7c9028f1a8e6432d50d5d67c3933aa
PatriciaBurgos <patriciaburgos.1999@gmail.com>
2020-08-17 20:08:34 +0200

    Constantes

---
+++ constantes.php
@@ -0,0 +1,9 @@
+<?php 
+
+	define('NOMBRE_MASCOTA', 'Niebla');
+	define('EDAD_MASCOTA', 3);
+	
+	echo "El nombre de mi mascota es: " . NOMBRE_MASCOTA . "<br />";
+	echo "La edad de mi mascota es: " . EDAD_MASCOTA;
+
+?>
