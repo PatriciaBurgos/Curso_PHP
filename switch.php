09c982ab418c6d169752d4c954018f6f06d570ba
PatriciaBurgos <patriciaburgos.1999@gmail.com>
2020-08-18 19:33:14 +0200

    Switch

---
+++ switch.php
@@ -0,0 +1,23 @@
+<?php
+
+	$cadena = "pera";
+
+	switch ($cadena) {
+		case 'manzana':
+			echo "Tu variable cadena es una Manzana";
+			break;
+
+		case 'platano':
+			echo "Tu variable cadena es un Platano";
+			break;
+
+		case 'pera':
+			echo "Tu variable cadena es una Pera";
+			break;
+		
+		default:
+			echo "No se el valor de tu variable cadena";
+			break;
+	}
+
+?>
